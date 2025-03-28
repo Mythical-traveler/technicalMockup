8243c59b9a5a5fd43747a5302305c32d7c5f2813
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2025-03-27 20:15:39 -0400

    New fix

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Welcome from './pages/Welcome.vue'
-import Notes from './pages/Notes.vue'
-import MoodBoard from './pages/MoodBoard.vue'
+import Welcome from '../pages/Welcome.vue'
+import Notes from '../pages/Notes.vue'
+import MoodBoard from '../pages/MoodBoard.vue'
 
 const routes = [
   { path: '/', component: Welcome },
