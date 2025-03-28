6bae141f4436f17b995aa0b9da8086b4c6cd8450
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2025-03-27 21:24:56 -0400

    Final working build for GitHub Pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+console.log(" main.js loaded — Vue app starting");
 
 createApp(App).use(router).mount('#app')
